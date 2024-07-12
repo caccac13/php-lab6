7c89f169cf0d679a6dee7f9b5acb38be6fd39648
Dang Hoang Hung <daghuglw@gmail.com>
2024-07-12 10:19:01 +0700

    Add files via upload

---
+++ ex1/db.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "php-lab5-ex1";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
